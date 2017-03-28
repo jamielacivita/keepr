ecc5a432cbdda14f7aa5cbdaee1e8631edfc2e87
brawlingNavada <JCLacivita@gmail.com>
2017-03-28 16:09:20 -0600

    basic flexbox stuff added.

--- www/src/components/readKeeps.vue
+++ www/src/components/readKeeps.vue
@@ -3,12 +3,33 @@
     <h1>Read Public Keeps Page</h1>
 
 <!--<h3>{{$root.store.state.keeps}}</h3>-->
-<ul id="example-2">
-  <li v-for="(item, index) in $root.store.state.keeps">
-    {{item.title}}
-  </li>
+
+<ul id="example-2" class = "fb">
+<li v-for="(item, index) in $root.store.state.keeps">
+<div class="card" style="width: 320px">
+<div class="cardOutline">
+  <img class="card-img-top" :src=item.imageUrl width="300px" alt="Card image cap">
+  <div class="card-block">
+    <h4 class="card-title">{{item.title}}</h4>
+    <p class="card-text">{{item.tags}}</p>
+    <a href="#" class="btn btn-primary">Keep</a>
+    <a href="#" class="btn btn-primary">SomeThing</a>
+    <a href="#" class="btn btn-primary">Else</a>
+  </div>
+</div>
+</div>
+</li>
 </ul>
     
+
+
+
+
+
+
+
+
+
   </div>
 </template>
 
@@ -55,4 +76,12 @@ li {
 a {
   color: #42b983;
 }
+
+.cardOutline {border: 1px solid black;
+padding: 15px; 
+box-shadow: 5px 5px 5px grey;
+}
+
+.fb {display:flex; flex-direction: row; border: 1px solid red; flex-wrap: wrap;}
+
 </style>
