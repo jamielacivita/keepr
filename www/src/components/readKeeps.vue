de233b9f53fc848f68e97b6e14c306e919791329
Jamie Lacivita <JCLacivita@gmail.com>
2017-03-28 20:06:32 -0600

    can primitavely add keeps to vaults.

--- www/src/components/readKeeps.vue
+++ www/src/components/readKeeps.vue
@@ -2,86 +2,123 @@
   <div class="readKeeps">
     <h1>Read Public Keeps Page</h1>
 
-<!--<h3>{{$root.store.state.keeps}}</h3>-->
+    <!--<h3>{{$root.store.state.keeps}}</h3>-->
 
-<ul id="example-2" class = "fb">
+    <ul id="publicKeeps" class="fb">
+      <li v-for="(item, index) in $root.store.state.keeps">
+        <div class="card" style="width: 320px">
+          <div class="cardOutline">
+            <img class="card-img-top" :src=item.imageUrl width="300px" alt="Card image cap">
+            <div class="card-block">
+              <h4 class="card-title">{{item.title}}</h4>
+              <p class="card-text">{{item.tags}}</p>
+              <!--<a href="#" class="btn btn-primary">Keep</a>-->
+              <a v-on:click="showId(item._id)" class="btn btn-primary">Vault Me.</a>
+              <!--<a href="#" class="btn btn-primary">Else</a>-->
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+
+    <div v-if="showKeepList" class="vaultBorder">
+      <h1> Space for Vault list here </h1>
+      <!--<h3>{{$root.store.state.vaults}}</h3>-->
+      <ul id="publicVaults">
+        <li v-for="(item, index) in $root.store.state.vaults">
+          <div class="card" style="width: 320px">
+            <div class="cardOutline">
+              <div class="card-block">
+                <h4 class="card-title">{{item.name}}</h4>
+                <p class="card-text">{{item.description}}</p>
+                <a v-on:click="addToVault(item._id)" class="btn btn-primary">Vault Me.</a>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+    </div>
 
 
-<li v-for="(item, index) in $root.store.state.keeps">
-<div class="card" style="width: 320px">
-<div class="cardOutline">
-  <img class="card-img-top" :src=item.imageUrl width="300px" alt="Card image cap">
-  <div class="card-block">
-    <h4 class="card-title">{{item.title}}</h4>
-    <p class="card-text">{{item.tags}}</p>
-    <!--<a href="#" class="btn btn-primary">Keep</a>-->
-    <a v-on:click="showId(item._id)" class="btn btn-primary">Vault Me.</a>
-    <!--<a href="#" class="btn btn-primary">Else</a>-->
-  </div>
-</div>
-</div>
-</li>
-
-</ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'readKeeps',
-  data () {
-    return {
-      showKeepList : false
+  export default {
+    name: 'readKeeps',
+    data() {
+      return {
+        showKeepList: false,
+        keepId: ''
+      }
+    },
+    methods:
+    {
+      getKeeps() {
+        this.$root.store.actions.getKeeps()
+      },
+      showId(keepId) {
+        console.debug("In showId with: ", keepId)
+        this.keepId = keepId;
+        this.showKeepList = !this.showKeepList;
+        //need to get a list of logged in users vaults.
+        //need to create a modal with users' vaults.
+      },
+
+      addToVault(vaultId) {
+        console.debug("In add to vault with keep Id of: ", this.keepId)
+        console.debug("In add to vault with vault Id of: ", vaultId)
+        this.$root.store.actions.setKeepToVault(this.keepId,  vaultId)
+
+
+      },
+
+
+
+
+    },
+    mounted: function () {
+      console.log("Mounting Read Keeps")
+      this.$root.store.actions.getKeeps()
+      this.$root.store.actions.getVaults()
     }
-  },
-methods: 
-{
-  getKeeps()
-  {
-  this.$root.store.actions.getKeeps()
-  },
-  showId(keepId)
-  {
-  console.debug("In showId with: ", keepId)
-  //need to get a list of logged in users vaults.
-  //need to create a modal with users' vaults.
 
   }
-},
-mounted: function()
-{
-  console.log("hello world.")
-  this.$root.store.actions.getKeeps()
-}
 
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-.cardOutline {border: 1px solid black;
-padding: 15px; 
-box-shadow: 5px 5px 5px grey;
-}
-
-.fb {display:flex; flex-direction: row; border: 1px solid red; flex-wrap: wrap;}
-
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
+  
+  .cardOutline {
+    border: 1px solid black;
+    padding: 15px;
+    box-shadow: 5px 5px 5px grey;
+  }
+  
+  .fb {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid red;
+    flex-wrap: wrap;
+  }
 </style>
--- www/src/store/index.js
+++ www/src/store/index.js
@@ -15,7 +15,7 @@ let base = axios.create({
 
 // REGISTER ALL DATA HERE
 let state = {
-  loginMessage : 'No one is logged in! - JWTO', 
+  loginMessage: 'No one is logged in! - JWTO',
   user: {},
   myVaults: {},
   myKeeps: {},
@@ -80,79 +80,112 @@ export default {
   // ALL DATA LIVES IN THE STATE
   state,
   // ACTIONS ARE RESPONSIBLE FOR MANAGING ALL ASYNC REQUESTS
-  actions: 
+  actions:
   {
-  register(register_object) {
-    console.debug("In the register method in the store.")
-  base.post('register', register_object)   
-  .then(res => {
-  //need to do some error checking for improper regristration. 
-  //the next two lines are setting the active state in the local store.
-  state.user = res.data.data
-  console.log("Error Code: ",res.data.error.code)
-  console.log("state.user: ", state.user)
-   }).catch(handleError)
+    register(register_object) {
+      console.debug("In the register method in the store.")
+      base.post('register', register_object)
+        .then(res => {
+          //need to do some error checking for improper regristration. 
+          //the next two lines are setting the active state in the local store.
+          state.user = res.data.data
+          console.log("Error Code: ", res.data.error.code)
+          console.log("state.user: ", state.user)
+        }).catch(handleError)
     },
 
-  login(login_object) {
-    console.log("In the login method in the store.")
+    login(login_object) {
+      console.log("In the login method in the store.")
 
-  base.post('login', login_object)  // <--- I believe this is a post to :3000/register with {body} 
-  .then(res => {
-  // //the next two lines are setting the active state in the local store.
-  console.debug(res)
-  //Was the login successful?
-  console.debug("res.data.error", res.data.error)
-  if (res.data.error == 'Invalid Email or Password')
-  {
-  console.debug("unsuccessful login")
-    //set improper login flag -- used to display message. 
-    state.loginMessage = 'Invalid email or password.'
-  }
-  else
-  {
-  console.debug("succesfully login.")
-  state.user = res.data.data
-  console.log("Res.data.data: ", res.data.data)
-  //send user to keeps page.
-  router.push({ path: 'readKeeps' })
-  }
-   }).catch(handleError)
+      base.post('login', login_object)  // <--- I believe this is a post to :3000/register with {body} 
+        .then(res => {
+          // //the next two lines are setting the active state in the local store.
+          console.debug(res)
+          //Was the login successful?
+          console.debug("res.data.error", res.data.error)
+          if (res.data.error == 'Invalid Email or Password') {
+            console.debug("unsuccessful login")
+            //set improper login flag -- used to display message. 
+            state.loginMessage = 'Invalid email or password.'
+          }
+          else {
+            console.debug("succesfully login.")
+            state.user = res.data.data
+            console.log("Res.data.data: ", res.data.data)
+            //send user to keeps page.
+            router.push({ path: 'readKeeps' })
+          }
+        }).catch(handleError)
     },
 
-  createKeep(obj_keep) {
+    createKeep(obj_keep) {
 
-  console.debug("In the createKeep method in the store with: ", obj_keep)
-  api.post('keeps', obj_keep)   
-  .then(res => {
-  //ToDo: Have to update the store after posting somehow.
-  //ToDo: Have to indicate that the keep was successfully created.  
-  console.log("Res: ",res)
-   }).catch(handleError)
+      console.debug("In the createKeep method in the store with: ", obj_keep)
+      api.post('keeps', obj_keep)
+        .then(res => {
+          //ToDo: Have to update the store after posting somehow.
+          //ToDo: Have to indicate that the keep was successfully created.  
+          console.log("Res: ", res)
+        }).catch(handleError)
     },
 
-  createVault(obj_vault) {
+    createVault(obj_vault) {
 
-  console.debug("In the createVault method in the store with: ", obj_vault)
-  api.post('vaults', obj_vault)   
-  .then(res => {
-  //ToDo: Have to update the store after posting somehow.
-  //ToDo: Have to indicate that the keep was successfully created.  
-  console.log("Res: ",res)
-   }).catch(handleError)
+      console.debug("In the createVault method in the store with: ", obj_vault)
+      api.post('vaults', obj_vault)
+        .then(res => {
+          //ToDo: Have to update the store after posting somehow.
+          //ToDo: Have to indicate that the keep was successfully created.  
+          console.log("Res: ", res)
+        }).catch(handleError)
     },
 
-  getKeeps() {
-  console.log("In the get Keeps method in the store.")
-  api.get('keeps')   
-  .then(res => {
-  console.log("Res from Get Keeps: ", res.data.data)
-  console.log("Current state of store: ", state.keeps)
-  console.log("updating store state.")
-  state.keeps = res.data.data;
-  console.log("New state of store: ", state.keeps)
-   }).catch(handleError)
+    getKeeps() {
+      //console.log("In the get Keeps method in the store.")
+      api.get('keeps')
+        .then(res => {
+          //console.log("Res from Get Keeps: ", res.data.data)
+          //console.log("Current state of store: ", state.keeps)
+          //console.log("updating store state.")
+          state.keeps = res.data.data;
+          //console.log("New state of store: ", state.keeps)
+        }).catch(handleError)
+    },
+
+    getVaults() {
+      console.log("In the get Vaults method in the store.")
+      api.get('vaults')
+        .then(res => {
+          state.vaults = res.data.data;
+        }).catch(handleError)
+    },
+
+    setKeepToVault(keepId, vaultId) {
+      console.log("In set keep to vault in the store.")
+      console.debug("In setKeepToVault with keep Id of: ", keepId)
+      console.debug("In setKeepToVault with vault Id of: ", vaultId)
+
+      //perform a put to add the keep Id to the vault.
+      //First pull in the current vault object.
+      api.get('vaults/' + vaultId)
+        .then(res => {
+          console.log("before adding keepId: ", res.data.data);
+          let keeps = res.data.data.keeps
+          console.log("Keeps before adding keepId: ", res.data.data.keeps);
+          //add the new keepId to the keeps array.
+          res.data.data.keeps.push(keepId)
+          console.log("Keeps after adding keepId: ", res.data.data.keeps);
+          //Put the new array in the database. 
+
+          let obj_keep = {}
+          obj_keep.keeps = res.data.data.keeps
+
+          api.put('vaults/' + vaultId, obj_keep)
+            .then(res => {
+              state.vaults = res.data.data;
+            }).catch(handleError)
+        }).catch(handleError)
     },
   }
 }
- 
+
